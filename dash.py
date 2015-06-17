1ea2cbc91b19d04287d31660d15ac337e819db1e
Jonathan Zrake <jonathan.zrake@gmail.com>
2015-06-17 09:45:56 -0700

    Add the python FFE run viewer stuff

---
+++ dash.py
@@ -0,0 +1,354 @@
+#!/usr/bin/env python
+
+
+import os
+import argparse
+
+try: import wx
+except ImportError:
+    print "Problem: wxPython is a dependency"
+
+
+
+class HasListeners(object):
+    def __init__(self):
+        self.listeners = [ ]
+
+    def add_listener(self, listener):
+        self.listeners.append(listener)
+
+    def notify_listeners(self):
+        for listener in self.listeners:
+            listener.receive_change(self)
+
+
+
+class UserConfiguration(wx.Panel, HasListeners):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        HasListeners.__init__(self)
+
+        self.widgets = { }
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self.sizer)
+
+
+    def add_boolean(self, label, default, alias=None):
+        if alias is None: alias = label
+        cb = wx.CheckBox(self, label=label)
+        cb.SetValue(default)
+        cb.Bind(wx.EVT_CHECKBOX, self.value_changed)
+        self.sizer.Add(cb)
+        self.sizer.Layout()
+        self.widgets[alias] = cb
+
+
+    def add_choices(self, label, choices, default=None, alias=None, radio=False):
+        if alias is None: alias = label
+
+        if radio:
+            cb = wx.RadioBox(self, label=label, choices=choices, style=wx.RA_SPECIFY_ROWS)
+            cb.SetSelection(choices.index(default))
+            cb.Bind(wx.EVT_RADIOBOX, self.value_changed)
+        else:
+            cb = wx.ComboBox(self, style=wx.CB_READONLY, name=label, choices=choices)
+            cb.Bind(wx.EVT_COMBOBOX, self.value_changed)
+            cb.SetValue(default if default is not None else choices[0])
+
+        self.sizer.Add(cb)
+        self.sizer.Layout()
+        self.widgets[alias] = cb
+
+
+    def value_changed(self, event):
+        self.notify_listeners()
+
+
+    def get_configuration(self):
+        cfg = dict()
+        for alias in self.widgets:
+            try:
+                cfg[alias] = self.widgets[alias].GetValue()
+            except AttributeError:
+                n = self.widgets[alias].GetSelection()
+                cfg[alias] = self.widgets[alias].GetString(n)
+        return cfg
+
+
+
+class DataSourcePanel(wx.Panel, HasListeners):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        HasListeners.__init__(self)
+
+        self.listbox_run = wx.ListBox(self)
+        self.listbox_chk = wx.ListBox(self)
+        self.listbox_ana = wx.ListBox(self)
+
+        self.listbox_run.Bind(wx.EVT_LISTBOX, self.evt_listbox_run)
+        self.listbox_chk.Bind(wx.EVT_LISTBOX, self.evt_listbox_chk)
+        self.listbox_ana.Bind(wx.EVT_LISTBOX, self.evt_listbox_ana)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.listbox_run, 1, wx.GROW)
+        sizer.Add(self.listbox_chk, 1, wx.GROW)
+        sizer.Add(self.listbox_ana, 1, wx.GROW)
+        self.SetSizer(sizer)
+
+
+    def load_run(self, rundir, load_chk=True, load_ana=True):
+        runid = os.path.basename(rundir)
+
+        has_ffe_dat = os.path.isfile(os.path.join(rundir, 'ffe.dat'))
+        has_analysis_h5 = os.path.isfile(os.path.join(rundir, 'analysis.h5'))
+
+        if not (has_ffe_dat and has_analysis_h5):
+            print "Not a valid run directory", rundir
+        elif self.listbox_run.FindString(runid) != -1:
+            self.make_run_active(rundir) # will reload checkpoints
+        else:
+            self.listbox_run.Append(runid, clientData=rundir)
+
+
+    def make_run_active(self, rundir):
+        import pickle
+        import h5py
+
+
+        analysis_h5 = os.path.join(rundir, "analysis.h5")
+        manifest_pk = os.path.join(rundir, "manifest.pk")
+
+        prev_chk = self.listbox_chk.GetSelection()
+        prev_ana = self.listbox_ana.GetSelection()
+
+        self.listbox_chk.Clear()
+        self.listbox_ana.Clear()
+
+        for thing in os.listdir(rundir):
+            if thing.startswith('chkpt.') and thing.endswith('.h5'):
+                self.listbox_chk.Append(thing)
+
+        try:
+            groups = pickle.load(open(manifest_pk, 'r'))
+        except:
+            print "caching analysis groups for", rundir
+            groups = [ ]
+            h5f = h5py.File(analysis_h5, "r")
+            for group in h5f:
+                print group
+                groups.append(group)
+            h5f.close()
+            pickle.dump(groups, open(manifest_pk, 'w'))
+
+        for group in groups:
+            self.listbox_ana.Append(group)
+
+        if prev_ana != -1 and prev_ana < self.listbox_ana.GetCount():
+            self.listbox_ana.SetSelection(prev_ana)
+        if prev_chk != -1 and prev_chk < self.listbox_chk.GetCount():
+            self.listbox_chk.SetSelection(prev_chk)
+
+
+    def get_rundir(self):
+        n = self.listbox_run.GetSelection()
+        if n < 0:
+            return None
+        else:
+            return self.listbox_run.GetClientData(n)
+
+
+    def get_checkpoint(self):
+        rundir = self.get_rundir()
+        if rundir is None:
+            return None
+        else:
+            n = self.listbox_chk.GetSelection()
+            if n < 0:
+                return None
+            else:
+                return os.path.join(rundir, self.listbox_chk.GetString(n))
+            
+
+    def get_analysis_group(self):
+        rundir = self.get_rundir()
+        if rundir is None:
+            return None
+        else:
+            n = self.listbox_ana.GetSelection()
+            if n < 0:
+                return None
+            else:
+                return self.listbox_ana.GetString(n)
+
+
+
+    """Callbacks bound to things
+
+    """
+    def evt_listbox_run(self, event):
+        rundir = self.get_rundir()
+        if rundir is not None:
+            self.make_run_active(rundir)
+            self.notify_listeners()
+
+
+    def evt_listbox_chk(self, event):
+        self.notify_listeners()
+
+
+    def evt_listbox_ana(self, event):
+        self.notify_listeners()
+
+
+
+class MainWindow(wx.Frame):
+
+    def __init__(self, directories):
+        wx.Frame.__init__(self, None, -1, 'FFE Dashboard')
+
+        self.directories = directories
+        self.import_matplotlib()
+        self.create_panel()
+
+
+    def import_matplotlib(self):
+        import matplotlib
+
+        matplotlib.use('WXAgg')
+
+        from matplotlib.figure import Figure
+        from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
+        from matplotlib.backends.backend_wxagg import NavigationToolbar2WxAgg
+
+        self.Figure = Figure
+        self.FigureCanvas = FigureCanvasWxAgg
+        self.NavigationToolbar = NavigationToolbar2WxAgg
+
+
+    def create_panel(self):
+        import matplotlib.pyplot as plt
+
+        panel = wx.Panel(self)
+
+        figure = self.Figure([6, 6])
+        canvas = self.FigureCanvas(panel, -1, figure)
+        navbar = self.NavigationToolbar(canvas)
+
+
+        import plots
+        self.plots = { }
+        self.plots[      'TimeSeriesPlot'] = plots.      TimeSeriesPlot(figure)
+        self.plots[     'SingleImagePlot'] = plots.     SingleImagePlot(figure)
+        self.plots[   'PowerSpectrumPlot'] = plots.   PowerSpectrumPlot(figure)
+        self.plots[  'AlphaHistogramPlot'] = plots.  AlphaHistogramPlot(figure)
+
+        for plot in self.plots.values():
+            user_configuration = UserConfiguration(panel)
+            user_configuration.Hide()
+            user_configuration.add_listener(self)
+            plot.create_configuration(user_configuration)
+            plot.user_configuration = user_configuration
+
+
+        panel.data_source = DataSourcePanel(panel)
+        panel.canvas = canvas
+        panel.navbar = navbar
+        panel.button_refresh = wx.Button(panel, label="Refresh directories")
+        panel.button_recache = wx.Button(panel, label="Recache currrent run")
+        panel.radio_plots = wx.RadioBox(panel, label="", choices=self.plots.keys(),
+                                        style=wx.RA_SPECIFY_ROWS)
+
+
+
+        panel.button_refresh.Bind(wx.EVT_BUTTON, self.refresh_directories)
+        panel.button_recache.Bind(wx.EVT_BUTTON, self.recache_current_run)
+        panel.radio_plots.Bind(wx.EVT_RADIOBOX, self.on_radio_plots)
+        panel.data_source.add_listener(self)
+
+        vsizer = wx.BoxSizer(wx.VERTICAL)
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        vsizer.Add(panel.navbar)
+        vsizer.Add(panel.button_refresh)
+        vsizer.Add(panel.button_recache)
+        vsizer.Add(panel.radio_plots)
+
+        for plot in self.plots.values():
+            vsizer.Add(plot.user_configuration, 1, wx.EXPAND)
+
+        hsizer.Add(panel.data_source, 1, wx.EXPAND)
+        hsizer.Add(vsizer, 0, wx.EXPAND)
+        hsizer.Add(panel.canvas, 4, wx.EXPAND)
+        panel.SetSizer(hsizer)
+        hsizer.Fit(self)
+
+
+        self.figure = figure
+        self.panel = panel
+        self.plot = None
+        self.on_radio_plots()
+        self.refresh_directories()
+
+
+    def switch_active_plot(self, plot_name):
+        
+        plot = self.plots[plot_name]
+        plot.set_data_source(self.panel.data_source)
+
+        if self.plot is not None: self.plot.user_configuration.Hide()
+        plot.user_configuration.Show()
+
+        self.panel.Layout()
+
+        for ax in self.figure.axes: self.figure.delaxes(ax)
+        for ax in plot.get_axes(): self.figure.add_axes(ax)
+        self.panel.canvas.draw()
+        self.plot = plot
+
+
+    def receive_change(self, broadcaster):
+
+        if self.plot is None: return
+
+        if broadcaster is self.panel.data_source:
+            self.plot.set_data_source(self.panel.data_source)
+            self.panel.canvas.draw()
+        elif broadcaster is self.plot.user_configuration:
+            cfg = self.plot.user_configuration.get_configuration()
+            self.plot.configure(**cfg)
+            self.panel.canvas.draw()
+
+
+    def on_radio_plots(self, event=None):
+        n = self.panel.radio_plots.GetSelection()
+        plot_name = self.panel.radio_plots.GetString(n)
+        self.switch_active_plot(plot_name)
+
+
+    def refresh_directories(self, event=None):
+        for directory in self.directories:
+            #for runid in os.listdir(directory):
+            #self.panel.data_source.load_run(os.path.join(directory, runid))
+            self.panel.data_source.load_run(directory)
+
+
+    def recache_current_run(self, event=None):
+        rundir = self.panel.data_source.get_rundir()
+        try: os.remove(os.path.join(rundir, 'tseries.pk'))
+        except OSError: pass
+        try: os.remove(os.path.join(rundir, 'manifest.pk'))
+        except OSError: pass
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="FFE run viewer --- needs: numpy, h5py, matplotlib, wxPython")
+    parser.add_argument("rundir", nargs="*")
+
+    args = parser.parse_args()
+
+    app = wx.App(False)
+    app.frame = MainWindow(args.rundir)
+    app.frame.Show()
+    app.MainLoop()
---
+++ plots.py
@@ -0,0 +1,347 @@
+
+
+import os
+
+
+def load_tseries_data(rundir, recache=False, tmin=0.01, tmax=None):
+    """Load a pickle of a time series from the disk if it exists, otherwise load the
+    data from the text file and create the pickle
+
+    """
+    import pickle
+    import numpy as np
+    import h5py
+
+    pickle_name = '%s/tseries.pk' % rundir
+
+    if os.path.isfile(pickle_name) and not recache:
+        print "load from pickle...", rundir
+        return pickle.load(open(pickle_name, 'r'))
+    else:
+        print "no pickle, caching data"
+
+
+    h5f = h5py.File("%s/analysis.h5" % rundir, 'r')
+    ffe_dat = np.loadtxt("%s/ffe.dat"% rundir)
+
+    tk = [ ]
+    k_X = { }
+
+    k_X['electric'] = [ ]
+    k_X['magnetic'] = [ ]
+    k_X['helicity'] = [ ]
+
+    k_E, k_B, k_H = k_X['electric'], k_X['magnetic'], k_X['helicity']
+
+    for group in h5f:
+
+        ti = float(h5f[group]['magnetic'].attrs['fullname'])
+        if tmin is not None and ti < tmin: continue
+        if tmax is not None and ti > tmax: continue
+
+        print group, ti
+        tk.append(ti)
+
+        for which in k_X:
+            try:
+                k = h5f[group][which]['binlocX'][:]
+                P = h5f[group][which]['binval' ][:]
+                k_X[which].append( (P * k).mean() / P.mean() )
+            except KeyError as e:
+                print group, e
+
+    h5f.close()
+
+    tk = np.array(tk)
+    pickle.dump([tk, k_X, ffe_dat], open(pickle_name, 'w'))
+    return tk, k_X, ffe_dat
+
+
+
+class TimeSeriesPlot(object):
+
+
+    def __init__(self, figure):
+        """Should not modify the figure instance, i.e. do not leave any new axes
+        attached to it, as add_subplot would do
+
+        """
+        from matplotlib import lines
+        from matplotlib import axes
+
+        self.magnetic = lines.Line2D([], [])
+        self.electric = lines.Line2D([], [])
+        self.helicity = lines.Line2D([], [])
+        self.monopole = lines.Line2D([], [])
+
+        self.ax1 = axes.Axes(figure, [0.1, 0.1, 0.8, 0.8])
+        self.ax1.add_line(self.magnetic)
+        self.ax1.add_line(self.electric)
+        self.ax1.add_line(self.helicity)
+        self.ax1.add_line(self.monopole)
+        self.configure()
+
+
+    def get_axes(self):
+        return [self.ax1]
+
+
+    def set_data_source(self, data_source):
+        """Heavier re-plot; may re-load data from the disk
+
+        """
+        rundir = data_source.get_rundir()
+        if rundir is None: return
+        tk, k_X, ffe_dat = load_tseries_data(rundir)
+
+        t  = ffe_dat[:,0]
+        Ue = ffe_dat[:,1]
+        Ub = ffe_dat[:,2]
+        Hm = ffe_dat[:,3]
+        Bm = ffe_dat[:,4]
+
+        Ub /= Ub[0]
+
+        self.magnetic.set_data(t, Ub)
+        self.electric.set_data(t, Ue)
+        self.helicity.set_data(t, abs(Hm))
+        self.monopole.set_data(t, abs(Bm))
+        self.ax1.relim(visible_only=True)
+        self.ax1.autoscale_view()
+
+        
+    def configure(self, **configuration):
+        """Light re-plot; will only issue fast reconfigurations
+
+        """
+        self.magnetic.set_color('blue')
+        self.electric.set_color('green')
+        self.helicity.set_color('red')
+        self.monopole.set_color('purple')
+        self.magnetic.set_visible(configuration.get('magnetic', True))
+        self.electric.set_visible(configuration.get('electric', True))
+        self.helicity.set_visible(configuration.get('helicity', False))
+        self.monopole.set_visible(configuration.get('monopole', False))
+        self.ax1.set_xscale('log' if configuration.get('logx', False) else 'linear')
+        self.ax1.set_yscale('log' if configuration.get('logy', False) else 'linear')
+
+
+    def create_configuration(self, user_config):
+        user_config.add_boolean("magnetic", True)
+        user_config.add_boolean("electric", True)
+        user_config.add_boolean("helicity", True)
+        user_config.add_boolean("monopole", True)
+        user_config.add_boolean("log x", False, alias='logx')
+        user_config.add_boolean("log y", False, alias='logy')
+
+
+
+class SingleImagePlot(object):
+    def __init__(self, figure):
+        from matplotlib import image
+        from matplotlib import axes
+
+        self.ax1 = axes.Axes(figure, [0.0, 0.0, 1.0, 1.0])
+        self.image = image.AxesImage(self.ax1)
+        self.image.set_data([[],[]])
+        self.ax1.add_image(self.image)
+
+        self.chkpt = None
+        self.field = 'B3'
+
+        self.configure()
+
+
+    def get_axes(self):
+        return [self.ax1]
+    
+    
+    def reload_data(self, chkpt, field):
+        import h5py
+        import numpy as np
+        if chkpt == self.chkpt and field == self.field: return
+        if chkpt is None or field is None:
+            data = np.array([[0,0], [0,0]])
+        else:
+            which = dict(E='electric', B='magnetic', J='electric_current')[field[0]]
+            h5f = h5py.File(chkpt, 'r')
+            data = h5f[which][field][...].T
+            h5f.close()
+        self.image.set_data(data)
+        if not self.fix_clim:
+            self.image.set_clim(data.min(), data.max())
+        self.ax1.set_xlim(0, data.shape[1]-1)
+        self.ax1.set_ylim(0, data.shape[0]-1)
+        self.chkpt = chkpt
+        self.field = field
+
+
+    def set_data_source(self, data_source):
+        """Heavier re-plot; may re-load data from the disk
+
+        """
+        rundir = data_source.get_rundir()
+        chkpt = data_source.get_checkpoint()
+        self.reload_data(chkpt, self.field)
+
+
+    def configure(self, **configuration):
+        self.fix_clim = configuration.get('fix_clim', False)
+        self.image.set_cmap(configuration.get('cmap', 'jet'))
+        self.ax1.xaxis.set_visible(configuration.get('draw_axes', False))
+        self.ax1.yaxis.set_visible(configuration.get('draw_axes', False))
+        field = configuration.get('field', 'B3')
+        self.reload_data(self.chkpt, field)
+
+
+    def create_configuration(self, user_config):
+        import matplotlib
+        cmaps = matplotlib.cm.datad.keys()
+        user_config.add_boolean('Fix limits', default=False, alias='fix_clim')
+        user_config.add_choices("Color map", cmaps, default='jet', alias='cmap')
+        user_config.add_choices("Field",
+                                ['E1', 'E2', 'E3',
+                                 'B1', 'B2', 'B3',
+                                 'J1', 'J2', 'J3'], default='B3', alias='field', radio=True)
+
+
+
+class PowerSpectrumPlot(object):
+    def __init__(self, figure):
+        from matplotlib import lines
+        from matplotlib import axes
+
+        self.ax1 = axes.Axes(figure, [0.1, 0.1, 0.8, 0.8])
+        self.electric = lines.Line2D([], [], lw=2, color='green')
+        self.magnetic = lines.Line2D([], [], lw=2, color='blue')
+        self.helicity = lines.Line2D([], [], lw=2, color='red')
+
+        self.ax1.add_line(self.electric)
+        self.ax1.add_line(self.magnetic)
+        self.ax1.add_line(self.helicity)
+
+        self.ax1.set_xscale('log')
+        self.ax1.set_yscale('log')
+
+        self.configure()
+
+
+    def get_axes(self):
+        return [self.ax1]
+    
+
+    def set_data_source(self, data_source):
+        import h5py
+        rundir = data_source.get_rundir()
+        group = data_source.get_analysis_group()
+        if rundir is None or group is None: return
+
+        analysis_h5 = os.path.join(rundir, 'analysis.h5')
+        h5f = h5py.File(analysis_h5, 'r')
+
+        for which in ['electric', 'magnetic', 'helicity']:
+            k = h5f[group][which]['binlocX'][:]
+            P = h5f[group][which]['binval'][:]
+            getattr(self, which).set_data(k, P)
+
+        self.ax1.relim()
+        self.ax1.autoscale_view()
+
+        h5f.close()
+
+        
+    def configure(self, **configuration):
+        pass
+
+
+    def create_configuration(self, user_config):
+        pass
+
+
+
+class AlphaHistogramPlot(object):
+    def __init__(self, figure):
+        from matplotlib import lines
+        from matplotlib import axes
+
+        self.alp = lines.Line2D([], [], lw=1, color='k')
+        self.ax1 = axes.Axes(figure, [0.1, 0.1, 0.8, 0.8])
+        self.ax1.add_line(self.alp)
+        self.ax1.set_xscale('linear')
+        self.ax1.set_yscale('log')
+
+        self.data_source = None
+
+        self.downsample_factor = 0
+        self.configure()
+
+
+    def get_axes(self):
+        return [self.ax1]
+
+
+    def set_data_source(self, data_source=None):
+        import h5py
+
+        if data_source is None: data_source = self.data_source
+        if data_source is None: return
+        self.data_source = data_source
+
+        if self.checkpoint:
+            chkpt = data_source.get_checkpoint()
+
+            if chkpt is None:
+                return
+            else:
+                group = 'spectra'
+                h5f = h5py.File(chkpt, 'r')
+
+        else:
+            group = data_source.get_analysis_group()
+
+            if group is None:
+                return
+            else:
+                rundir = data_source.get_rundir()
+                analysis_h5 = os.path.join(rundir, 'analysis.h5')
+                h5f = h5py.File(analysis_h5, 'r')
+
+        try:
+            d = lambda A: self.downsample(A, self.downsample_factor)
+            alp_e = h5f[group]['alpha-hist']['binedgX'][:]
+            alp_x = h5f[group]['alpha-hist']['binlocX'][:]
+            alp_y = h5f[group]['alpha-hist']['binval'][:]
+
+            dxa = alp_e[1:] - alp_e[:-1]
+            tota = (dxa * alp_y).sum() # not correct, cow binmode set to 'counts'
+            self.alp.set_data(d(alp_x), d(alp_y) / tota)
+
+        except KeyError: # There was no data
+            print "There was no alpha data"
+            self.alp.set_data([], [])
+
+        h5f.close()
+
+        self.ax1.relim()
+        self.ax1.autoscale_view()
+
+
+    def configure(self, **configuration):
+        self.checkpoint = configuration.get('checkpoint', False)
+        self.downsample_factor = int(configuration.get('downsample', 0))
+        self.set_data_source()
+
+
+    def create_configuration(self, user_config):
+        user_config.add_boolean('checkpoint', False)
+        user_config.add_choices('downsample', ['0', '1', '2', '3', '4'])
+
+
+    def downsample(self, A, factor):
+        """
+        Combine 2^factor adjacent freqency bins
+        """
+        if factor == 0:
+            return A
+        else:
+            return self.downsample(0.5 * (A[0:-2:2] + A[1:-1:2]), factor-1)
